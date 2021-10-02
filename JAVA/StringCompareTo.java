ccf5da40853eb306dc26dbb7c829e945a60f09df
Harshit Mody <55501384+harshitmody72@users.noreply.github.com>
2021-10-02 20:19:41 +0530

    Create StringCompareTo.java
    Adding String compareTo()

---
+++ JAVA/StringCompareTo.java
@@ -0,0 +1,14 @@
+class Main {
+  public static void main(String[] args) {
+    String str1 = "Learn Java";
+    String str2 = "Learn Kolin";
+    int result;
+
+    // comparing str1 with str2
+    result = str1.compareTo(str2);
+
+    System.out.println(result);
+  }
+}
+
+// Output: -1
